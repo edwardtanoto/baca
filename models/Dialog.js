7f61808b0f3ff37d1279838ca5e8f1d3af1b4f43
Edward Tanoto <ed27tanoto@gmail.com>
2020-08-14 21:02:08 +0700

    uupdate

---
+++ models/Dialog.js
@@ -0,0 +1,22 @@
+/**
+ * DIALOG
+ * CHARACTER
+ * TEXTS
+ * GAMBAR
+ * INDICATOR->TAU KALO SETELAHNYA QUIZ?
+ */
+
+class Dialog {
+    constructor(){
+
+    }
+    
+}
+
+// {
+//     "Ibu guru": Selamat pagi anak2
+// "Budi" : Selamat pagi bu!
+// "Ibu guru":  Kita akan memulai kelas hari ini dengan perkenalan dulu ya, mulai dari kamu.
+// 'Budi': Saya Budi ibu.
+
+// }
