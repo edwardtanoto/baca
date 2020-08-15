973f8108054a75987c23d23cd442a0914587a6cb
ferdyanggara <moeharyoso89@gmail.com>
2020-08-15 16:47:26 +0800

    bug fixed

--- screen/Story.js
+++ screen/Story.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -9,82 +9,81 @@ import {
   TouchableOpacity,
   FlatList,
   TouchableWithoutFeedback,
-} from "react-native";
+} from 'react-native';
 
-import {SCRIPT as script} from '../data/Script' 
+import { SCRIPT as script } from '../data/Script';
 
 export const Story = () => {
-    let [index, setIndex] = useState(0)
-   
-    const nextStory = () => {
-        console.log(script[index].characterName)
-        setIndex(index++)
+  const [index, setIndex] = useState(0);
+  const nextStory = () => {
+    if (index < script.length - 1) {
+      let tempIndex = index + 1;
+      setIndex(tempIndex);
     }
-    console.log(script[index].characterName)
+  };
+  // console.log(script[index].characterName);
   return (
-    
-    <View style={{ backgroundColor: "white", flex: 1, justifyContent: "space-evenly"}}>
-       
+    <View
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+        justifyContent: 'space-evenly',
+      }}
+    >
       <ImageBackground
-        source={require("../assets/classroom.jpg")}
+        source={require('../assets/classroom.jpg')}
         style={styles.image}
       >
-          <View >
+        <View>
           <Image
-        source={require("../assets/ibu.png")}
-        style={styles.character}
-      ></Image>
-      </View>
-      <TouchableOpacity style={styles.dialog} onPress={nextStory}>
-      {/* <View >
-        <Text style={styles.Tagline}>{script[index].characterName}</Text>
-        <Text style={styles.Textname}>
-        {script[index].dialog}
-        </Text>
-      </View> */}
-      <View >
-        <Text style={styles.Tagline}>anaananana</Text>
-        <Text style={styles.Textname}>
-        scriptbcbcbcbcb        
-        </Text>
-      </View>
-      </TouchableOpacity>
+            source={require('../assets/ibu.png')}
+            style={styles.character}
+          ></Image>
+        </View>
+        <TouchableOpacity style={styles.dialog} onPress={nextStory}>
+          <View>
+            <Text style={styles.Tagline}>{script[index].characterName}</Text>
+            <Text style={styles.Textname}>{script[index].dialog}</Text>
+          </View>
+          {/* <View>
+            <Text style={styles.Tagline}>anaananana</Text>
+            <Text style={styles.Textname}>scriptbcbcbcbcb</Text>
+          </View> */}
+        </TouchableOpacity>
       </ImageBackground>
     </View>
-    
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   image: {
     flex: 1,
-    height: "100%",
-    justifyContent: "flex-end",
-    
+    height: '100%',
+    justifyContent: 'flex-end',
   },
   character: {
-    height : '80%',
-    width :150
+    height: '80%',
+    width: 150,
   },
   Tagline: {
-    color: "black",
+    color: 'black',
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 20,
   },
   dialog: {
-    justifyContent: "flex-end",
+    justifyContent: 'flex-end',
     padding: 25,
     position: 'absolute',
     zIndex: 10,
     // marginTop: 300,
     width: '100%',
-    backgroundColor :'rgba(255,255,255,0.5)',
+    backgroundColor: 'rgba(255,255,255,0.5)',
   },
 });
